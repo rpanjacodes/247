f8584f2967ba314db732490777d7b2ff34789099
r.panja codes <101450382+rpanjacodes@users.noreply.github.com>
2025-02-13 08:22:44 +0530

    Update stream.js
    Updated from Prev

--- stream.js
+++ stream.js
@@ -1,47 +1,67 @@
+const fs = require("fs");
+
 const { exec } = require("child_process");
 
-const YOUTUBE_STREAM_KEY = "your-key";
+const ytdl = require("youtube-dl-exec");
 
-const IMAGE_PATH = "background.jpg";
+const { fetchPlaylist } = require("/home/container/fetchYoutube.js");
 
-async function startStream() {
+const { refreshCookies } = require("/home/container/autoRefreshCookies.js");
 
-    if (!YOUTUBE_STREAM_KEY) {
+const YOUTUBE_STREAM_KEY = "4jye-x93a-rkad-x7ap-e7fq";
 
-        console.error("Error: YOUTUBE_STREAM_KEY is not set.");
+const IMAGE_PATH = "static_image.jpg";
 
-        return;
+const FONT_PATH = "/home/container/font.ttf";
+
+let playlist = [];
+
+let currentIndex = 0;
+
+async function getNextSong() {
+
+    if (currentIndex >= playlist.length) {
+
+        console.log("🎵 Re-fetching playlist...");
+
+        await fetchPlaylist();
+
+        playlist = JSON.parse(fs.readFileSync("playlist.json"));
+
+        currentIndex = 0;
 
     }
 
-    console.log("Fetching songs from Spotify...");
-
-    const getSpotifyTracks = require("./fetchSpotify");
-
-    const tracks = await getSpotifyTracks();
-
-    if (tracks.length === 0) {
-
-        console.error("No tracks found in the Spotify playlist.");
-
-        return;
-
-    }
-
-    console.log(`Starting YouTube livestream with ${tracks.length} tracks...`);
-
-    // Updated FFmpeg command to ensure even height
-
-    const ffmpegCmd = `ffmpeg -re -loop 1 -i ${IMAGE_PATH} -vf "scale=iw:trunc(ih/2)*2" -f flv rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}`;
-
-    const process = exec(ffmpegCmd);
-
-    process.stdout.on("data", (data) => console.log(`[FFmpeg]: ${data}`));
-
-    process.stderr.on("data", (data) => console.error(`[FFmpeg Error]: ${data}`));
-
-    process.on("exit", (code) => console.log(`[FFmpeg] Exited with code ${code}`));
+    return playlist[currentIndex++];
 
 }
 
-module.exports = startStream;
+async function stream() {
+
+    await refreshCookies();
+
+    await fetchPlaylist();
+
+    playlist = JSON.parse(fs.readFileSync("playlist.json"));
+
+    while (true) {
+
+        const songUrl = await getNextSong();
+
+        console.log(`▶ Streaming: ${songUrl}`);
+
+        const ffmpegCmd = `ffmpeg -re -i "$(youtube-dl-exec -f bestaudio ${songUrl} -o -)" -loop 1 -i ${IMAGE_PATH} -vf "drawtext=text='Now Playing: ${songUrl}':fontfile=${FONT_PATH}:fontsize=24:fontcolor=white:x=10:y=10" -c:v libx264 -preset ultrafast -tune stillimage -c:a aac -b:a 128k -f flv rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}`;
+        
+const ffmpegProcess = exec(ffmpegCmd);
+
+        ffmpegProcess.stdout.on("data", (data) => console.log(data));
+
+        ffmpegProcess.stderr.on("data", (data) => console.error(data));
+
+        await new Promise((resolve) => ffmpegProcess.on("exit", resolve));
+
+    }
+
+}
+
+stream();
